f4bff9ca3c460141af6239e85d04e8dffd40fc00
Vetyugov <nickitavet1998@gmail.com>
2022-12-21 22:15:41 +0300

    Initial commit

---
+++ src/main/java/com/vetyugov/reactivespringservice/ReactiveSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vetyugov.reactivespringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetyugov/reactivespringservice/ReactiveSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetyugov.reactivespringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
